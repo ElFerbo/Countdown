da9809b71a2765141a5a7ca55d46e80258f59982
ElFerbo <57297160+ElFerbo@users.noreply.github.com>
2026-02-20 06:12:39 +0100

    Update TageBis.py

--- TageBis.py
+++ TageBis.py
@@ -11,11 +11,18 @@ atlanta_now = datetime.now(atlanta_tz)
 # --- Ziel-Datum (in Atlanta-Zeit) ---
 ziel_datum = atlanta_tz.localize(datetime(2026, 5, 30))
 
-# --- Countdown berechnen ---
-tage_bis = (ziel_datum - atlanta_now).days
+# --- Countdown berechnen --- 
+delta = ziel_datum - atlanta_now 
+tage_bis = delta.days
+tage = delta.days 
+stunden = delta.seconds // 3600 
+minuten = (delta.seconds % 3600) // 60
 
 st.subheader(f"Heute ist der {atlanta_now.strftime('%d.%m.%Y')}.")
 st.subheader(f"⏳ Es verbleiben noch {tage_bis} Tage bis zum 30.05.2026 !")
+#st.subheader(f"⏳ Es verbleiben noch {tage} Tage, {stunden} Stunden und {minuten} Minuten bis zum 30.05.2026 !")
+st.balloons()
+
 
 
 
